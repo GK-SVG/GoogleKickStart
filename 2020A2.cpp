38b397ae184a9466e859620217c964fadf917e34
gk-svg <gk32239@gmail.com>
2020-04-27 23:25:43 +0530

    change in array

---
+++ 2020A2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,k,p;
+        cin>>n>>k>>p;
+        int arr[n][k];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                cin>>arr[i][j]; 
+            }
+            
+        }
+        for (int i = 0; i < count; i++)
+        {
+            /* code */
+        }
+        
+    }
+    
+} 
